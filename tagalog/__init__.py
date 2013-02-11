1cc9642b6ce2db7c45ae058f0749662ff04ae60f
Nick Stenning <nick@whiteink.com>
2013-02-11 14:44:23 +0000

    Bump version -> v0.2.2

--- tagalog/__init__.py
+++ tagalog/__init__.py
@@ -5,7 +5,7 @@ import os
 from tagalog import io
 
 __all__ = ['io', 'stamp', 'tag']
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 # Use UTF8 for stdin, stdout, stderr
 os.environ['PYTHONIOENCODING'] = 'utf-8'
